c754fc5789cb4885836d37c8018254ebe92ff5a5
jdiboll13 <jdiboll@gmail.com>
2017-09-21 16:00:18 -0400

    Added Class1 and API works.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
 
 namespace ConsoleApp1
 {
@@ -6,7 +9,23 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = "http://api.openweathermap.org/data/2.5/weather?zip=33614,us&units=imperial&appid=0cf8cad19c95a8c1b81426766612471e";
+
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+
+            var rawResponse = String.Empty;
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                rawResponse = reader.ReadToEnd();
+            }
+
+            var weather = JsonConvert.DeserializeObject<RootObject>(rawResponse);
+
+            Console.WriteLine(weather.main.temp);
+            Console.ReadLine();
         }
     }
 }
